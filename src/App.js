2202949b120bcef591c8ad22cf24b782f8e5c67c
swanand2406 <swanand.sambhus@bluepineapple.io>
2022-04-14 23:47:56 +0530

    Added weather app assignment

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import Axios from "axios";
+import CityComponent from "./modules/CityComponent";
+import WeatherComponent from "./modules/WeatherInfoComponent";
+
+export const WeatherIcons = {
+  "01d": "/react-weather-app/icons/sunny.svg",
+  "01n": "/react-weather-app/icons/night.svg",
+  "02d": "/react-weather-app/icons/day.svg",
+  "02n": "/react-weather-app/icons/cloudy-night.svg",
+  "03d": "/react-weather-app/icons/cloudy.svg",
+  "03n": "/react-weather-app/icons/cloudy.svg",
+  "04d": "/react-weather-app/icons/perfect-day.svg",
+  "04n": "/react-weather-app/icons/cloudy-night.svg",
+  "09d": "/react-weather-app/icons/rain.svg",
+  "09n": "/react-weather-app/icons/rain-night.svg",
+  "10d": "/react-weather-app/icons/rain.svg",
+  "10n": "/react-weather-app/icons/rain-night.svg",
+  "11d": "/react-weather-app/icons/storm.svg",
+  "11n": "/react-weather-app/icons/storm.svg",
+};
+
+
+function App() {
+  const [city, updateCity] = useState();
+  const [weather, updateWeather] = useState();
+  const fetchWeather = async (e) => {
+    e.preventDefault();
+    const response = await Axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0140ef1cdcb20133eb13edfcd2e08304`,
+    );
+    updateWeather(response.data);
+  };
+  return (
+    <div className="Container">
+      <span className="span">React Weather App</span>
+      {city && weather ? (
+        <WeatherComponent weather={weather} city={city} />
+      ) : (
+        <CityComponent updateCity={updateCity} fetchWeather={fetchWeather} />
+      )}
+    </div>
+  );
+}
+
+export default App;
